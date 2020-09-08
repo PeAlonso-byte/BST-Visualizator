6b919619ebd7564d2c88244db654802849615ef6
PeAlonso-byte <65684738+PeAlonso-byte@users.noreply.github.com>
2020-09-08 17:52:37 +0200

    Adding TRAVERSAL ALGORITHMS

--- drawtree.py
+++ drawtree.py
@@ -1,4 +1,4 @@
-# https://leetcode.com/problems/recover-binary-search-tree/discuss/32539/Tree-Deserializer-and-Visualizer-for-Python
+# https://leetcode.com/problems/recover-binary-search-tree/discuss/32539/Tree-arrayToBSTr-and-Visualizer-for-Python
 import turtle
 class Node:
     def __init__(self, val, left=None, right=None):
@@ -7,18 +7,22 @@ class Node:
         self.right = right
     def __repr__(self):
         return 'Node({})'.format(self.val)
-    
-def deserialize(string):
+
+###########################################################
+#                       UTILITIES                         #
+###########################################################
+
+def arrayToBST(string):
     if string == '{}':
         return None
     nodes = [None if val == 'null' else Node(int(val))
              for val in string.strip('[]{}').split(',')]
-    kids = nodes[::-1]
-    # print(kids)
-    root = kids.pop()
+    arrayNodes = nodes[::-1]
+    # print(arrayNodes)
+    root = arrayNodes.pop()
     for node in nodes:
         if node:
-            if kids: insert(root, kids.pop())
+            if arrayNodes: insert(root, arrayNodes.pop())
     return root
 
 def insert(root,node): 
@@ -36,14 +40,31 @@ def insert(root,node):
             else: 
                 insert(root.left, node)
 
+
+###########################################################
+#                   TRAVERSAL ALGORITHMS                  #
+###########################################################
+
 def inorder(node):
-    import turtle
-    t = turtle.Turtle()
     if node: 
         inorder(node.left) 
         print(node.val) 
         inorder(node.right) 
+def preorder(node):
+    if node:
+        print(node.val)
+        preorder(node.left)
+        preorder(node.right)
 
+def postorder(node):
+    if node:
+        postorder(node.left)
+        postorder(node.right)
+        print(node.val)
+
+############################################################
+############################################################
+    
 # w is the width to draw the bst             
 def drawtree(root, w):
     def height(root):
@@ -53,10 +74,13 @@ def drawtree(root, w):
         t.penup()
         t.goto(x, y)
         t.pendown()
+    # node -> Node to draw
+    # x, y -> Coord of the node
+    # dx   -> Space between inline nodes.
     def draw(node, x, y, dx, bgColor, txtColor):
         if node:
             t.goto(x, y)
-            lineTo(x-7, y-20)      
+            lineTo(x-15, y-25)      
             t.fillcolor(bgColor)
             t.begin_fill()
             t.color(bgColor)
@@ -69,15 +93,24 @@ def drawtree(root, w):
             draw(node.left, x-dx, y-50, dx/2, bgColor, txtColor)
             lineTo(x-7, y-20)
             draw(node.right, x+dx, y-50, dx/2, bgColor, txtColor)
+            
     t = turtle.Turtle()
     t.speed(2); turtle.delay(1)
     h = height(root)
     lineTo(0, 30*h)
-    draw(root, 0, 30*h, 40*h, "green", "white")
+    draw(root, 0, 30*h, 30*h, "green", "white")
     t.hideturtle()
     turtle.mainloop()
-    
+
 if __name__ == '__main__':
-    root = deserialize('[50,30,20,40,70,60,80,25, 65]')
-    drawtree(root, -1)
+    #root = arrayToBST('[50,30,20,40,70,60,80,25,65,10,15,55,45]')
+    root = arrayToBST('[27,14,10,19,35,31,42]')
+    
+    print("##### INORDER #####")
     inorder(root)
+    print("##### PREORDER #####")
+    preorder(root)
+    print("##### POSTORDER #####")
+    postorder(root)
+
+    drawtree(root, -1)
